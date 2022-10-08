cddd7cd3963df346c642ae6243b0e256e1b303e5
Marco Antonio Gabriel - MGA <marco.gabriel@iteris.local>
2022-10-08 14:55:08 -0300

    CORRECAO DE VERSOES

--- src/test/java/com/moneyestudoapi/MoneyestudoApiApplicationTests.java
+++ src/test/java/com/moneyestudoapi/MoneyestudoApiApplicationTests.java
@@ -1,13 +1,12 @@
 package com.moneyestudoapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MoneyestudoApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
